e2bf03d206e3bfa895173dc9d3ffae59cc660e81
camenduru <54370274+camenduru@users.noreply.github.com>
2023-02-12 14:56:00 +0300

    in_container

--- main.py
+++ main.py
@@ -98,7 +98,7 @@ def main():
 def checks():
     "Check if the script is run from a virtual environment, if yes, check requirements"
 
-    if not is_root():
+    if not args.in_container:
         if not in_virtualenv():
             create_environment()
 
@@ -133,7 +133,7 @@ def checks():
 
     coloredlogs_install(level=args.log_level)
 
-    if is_root():
+    if args.in_container:
         # Check if we are up to date with the latest release
         version_check(commit_hash())
 
